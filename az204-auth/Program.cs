a5b9ec4be943b150146bec2fa6e5b5512cb047ca
Rob Brennan <rob@therobbrennan.com>
2023-07-25 10:46:52 -0700

    Add code for the interactive authentication

--- az204-auth/Program.cs
+++ az204-auth/Program.cs
@@ -2,3 +2,13 @@
 using Microsoft.Identity.Client;
 
 public static async Task Main(string[] args)
+
+// Add code for the interactive authentication
+private const string _clientId = "APPLICATION_CLIENT_ID";
+private const string _tenantId = "DIRECTORY_TENANT_ID";
+
+var app = PublicClientApplicationBuilder
+    .Create(_clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
