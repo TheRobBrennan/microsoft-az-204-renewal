d67523dbd4636cbdd9c585d1b131c2eb4f05e17d
Rob Brennan <rob@therobbrennan.com>
2023-07-25 10:48:49 -0700

    The contents of the Program.cs file should resemble the following example

--- az204-auth/Program.cs
+++ az204-auth/Program.cs
@@ -1,21 +1,25 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Identity.Client;
 
-public static async Task Main(string[] args)
+namespace az204_auth
+{
+  class Program
+  {
+    private const string _clientId = "APPLICATION_CLIENT_ID";
+    private const string _tenantId = "DIRECTORY_TENANT_ID";
 
-// Add code for the interactive authentication
-private const string _clientId = "APPLICATION_CLIENT_ID";
-private const string _tenantId = "DIRECTORY_TENANT_ID";
+    public static async Task Main(string[] args)
+    {
+      var app = PublicClientApplicationBuilder
+          .Create(_clientId)
+          .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+          .WithRedirectUri("http://localhost")
+          .Build();
+      string[] scopes = { "user.read" };
+      AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
 
-var app = PublicClientApplicationBuilder
-    .Create(_clientId)
-    .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
-    .WithRedirectUri("http://localhost")
-    .Build();
-
-// Acquire a token
-string[] scopes = { "user.read" };
-
-// Add code to request the token and write the result out to the console
-AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
-Console.WriteLine($"Token:\t{result.AccessToken}");
+      Console.WriteLine($"Token:\t{result.AccessToken}");
+    }
+  }
+}
